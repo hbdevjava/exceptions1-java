bf53be698e76b7ae29ce9aefdb00db2b7f225146
Hebert Brito <hbdev1211@gmail.com>
2022-05-25 11:14:32 -0300

    very bad solutiongit statusgit status

---
+++ src/TryCatch/application/Program.java
@@ -0,0 +1,50 @@
+package TryCatch.application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		method1();
+		/**try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e) { //Exceção de índice fora dos limite;
+			System.out.println("Invalid position!");
+		} catch (InputMismatchException e) {//Exceção de incompatibilidade de entrada
+			System.out.println("Input error");
+		}**/
+		System.out.println("End of program");
+		
+	}
+// ************** PILHA DE METODOS ***************
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
+		Scanner sc = new Scanner(System.in);
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e) { 
+			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
+		} catch (InputMismatchException e) {
+			System.out.println("Input error");
+		}
+		sc.close();
+		System.out.println("***METHOD2 END***");
+	}
+}
+		
+		
